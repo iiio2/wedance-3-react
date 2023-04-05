889e2841f8e5d8a97dfd216f47846839e136f467
Mr. Robot <gomexxxx71@gmail.com>
2023-04-05 18:08:34 +0600

    initial commit.

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h2>Hello</h2>
+      <button className="btn btn-primary">Button</button>
+    </div>
+  );
+}
+
+export default App;

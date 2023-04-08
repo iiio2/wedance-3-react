1fdc36780a18bd0122c5691064b1842be7392e3c
Mr. Robot <gomexxxx71@gmail.com>
2023-04-08 18:06:06 +0600

    add react router dom and build about page.

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,17 @@
-import "./App.css";
+import { Routes, Route } from "react-router-dom";
 import Navbar from "./components/common/Navbar";
+import About from "./components/About";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <div className="p-4">
+        <Routes>
+          <Route path="/about" element={<About />} />
+        </Routes>
+      </div>
     </div>
   );
 }

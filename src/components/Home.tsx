782d962a8265fba0605f7961e3d19c1ffb2c37b6
Mr. Robot <gomexxxx71@gmail.com>
2023-04-09 14:56:33 +0600

    refactor home component.

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,4 +1,6 @@
 import home from "../assets/home.jpg";
+import HomeContent from "./HomeContent";
+import GoogleLogin from "./common/GoogleLogin";
 
 const Home = () => {
   return (
@@ -11,13 +13,8 @@ const Home = () => {
       <div className="hero-overlay bg-opacity-60"></div>
       <div className="hero-content text-center text-neutral-content">
         <div className="max-w-md">
-          <h1 className="mb-5 text-5xl font-bold">Hello, WeDance</h1>
-          <p className="mb-5">
-            Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
-            excepturi exercitationem quasi. In deleniti eaque aut repudiandae et
-            a id nisi.
-          </p>
-          <button className="btn btn-primary">Get Started</button>
+          <HomeContent />
+          <GoogleLogin />
         </div>
       </div>
     </div>
---
+++ src/components/HomeContent.tsx
@@ -0,0 +1,14 @@
+const HomeContent = () => {
+  return (
+    <>
+      <h1 className="mb-5 text-5xl font-bold">Hello, WeDance</h1>
+      <p className="mb-5">
+        Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
+        excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a
+        id nisi.
+      </p>
+    </>
+  );
+};
+
+export default HomeContent;

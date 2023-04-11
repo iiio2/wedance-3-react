591b3ef13ce533400de39f6367af8fa4f3d19254
Mr. Robot <gomexxxx71@gmail.com>
2023-04-11 15:39:31 +0600

    get profile data.

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -10,6 +10,9 @@ interface Props {
 interface Person {
   displayName: string;
   email: string;
+  photoURL: string;
+  phoneNumber: number;
+  events: object[];
 }
 
 const Profile = ({ user }: Props) => {
@@ -31,10 +34,17 @@ const Profile = ({ user }: Props) => {
   }, [user.uid]);
 
   return (
-    <>
-      <p>Profile</p>
-      <p>{person.displayName}</p>
-    </>
+    <div className="profile p-4 text-neutral-950 leading-8 text-lg ">
+      <div className="avatar">
+        <div className="w-24 rounded-full">
+          <img src={person.photoURL} />
+        </div>
+      </div>
+      <h3 className="text-5xl">{person.displayName}</h3>
+      <p>{person.email}</p>
+      <p>{person.phoneNumber}</p>
+      {person.events && person.events.length === 0 && <p>No events founds.</p>}
+    </div>
   );
 };
 

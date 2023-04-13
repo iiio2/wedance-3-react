c7cff41225062edcd349e027a99106442cc35645
Mr. Robot <gomexxxx71@gmail.com>
2023-04-13 16:25:20 +0600

    fix css for default layout.

--- src/layouts/default.tsx
+++ src/layouts/default.tsx
@@ -6,7 +6,7 @@ interface Props {
 
 const DefaultLayout = ({ children }: Props) => {
   return (
-    <main className="p-4 text-justify text-neutral-950 leading-8 text-lg h-screen">
+    <main className="p-4 text-justify text-neutral-950 leading-8 text-lg min-h-screen">
       {children}
     </main>
   );

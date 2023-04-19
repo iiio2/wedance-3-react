f90d6ac60ea7c6d0cd8d5e51b746d0ccaedd29ac
Mr. Robot <gomexxxx71@gmail.com>
2023-04-19 13:24:58 +0600

    change structure of withSpinner HOC.

--- src/components/common/withSpinner.tsx
+++ src/components/common/withSpinner.tsx
@@ -1,31 +1,15 @@
-import { useState, useEffect } from "react";
+import useAuth from "../../hooks/useAuth";
+import Spinner from "./Spinner";
+import NotFound from "./NotFound";
 
 const withSpinner = (WrappedComponent: any) => {
   return (props: any) => {
-    const [loading, setLoading] = useState(true);
+    const { user, loading } = useAuth();
 
-    useEffect(() => {
-      setTimeout(() => {
-        setLoading(false);
-      }, 1500);
-    }, []);
+    if (loading) return <Spinner />;
+    if (!user) return <NotFound />;
+    if (!user.uid) return <NotFound />;
 
-    if (loading) {
-      return (
-        <div
-          style={{
-            display: "flex",
-            justifyContent: "center",
-            alignItems: "center",
-            height: "100vh",
-          }}
-        >
-          <div className="absolute right-1/2 bottom-1/2  transform translate-x-1/2 translate-y-1/2 ">
-            <div className="border-t-transparent border-solid animate-spin  rounded-full border-blue-400 border-8 h-64 w-64"></div>
-          </div>
-        </div>
-      );
-    }
     return <WrappedComponent {...props} />;
   };
 };
